8c033121932d272c9a0e34abdf1874a23040f41b
dplbsd <tuchalia@gmail.com>
2014-11-04 17:23:31 +0100

    Add basic comments

---
+++ memexplorer.py
@@ -0,0 +1,21 @@
+# N = # Data structures
+# M = # Mem Banks
+# Si = Size of DS i
+# Cj = Capacity of MV
+# Ei = Access cost if in memory bank
+# Ei*p = access cost if in external mem
+# O = # conflicts
+# Dk = cost of conflict k
+# Dk*p cost of conflict if in external memory
+# Yk = State of conflict can be:
+#		0 - If a and b DA are in different MB
+#		1 - If a and b DA are in the same MB
+#		p - If one is in EM
+#		2p - If both are in EM
+# Xij = Boolean 1 if i in MB j
+
+# Data:
+#	DataStructure = {'size', 'pos'}
+#	MemoryBank = {'capacity', 'pos'}
+#	Conflict
+
