98bbcde59231e62262191b9d9c529755a5486d71
apolitogaga <apolitogaga@gmail.com>
2014-11-04 19:42:14 +0100

    Latest update
    Here’s the latest part of the code

--- dev/memexplorer.py
+++ dev/memexplorer.py
@@ -32,11 +32,11 @@
 import random
 
 N = 200 # data structures
-M = 200 # mem banks
+M = 300 # mem banks
 O =  4 # conflicts
 
 u = [0]*(M+1) # u_j >  capacity used <list>
-s = [0]*(M+1) # s_i  capacity of data structure
+s = [0]*(N) # s_i  capacity of data structure
 c = [0]*(M+1) # c_i  capacity of memory bank
 X = [[0]*(M+1)]*N # x_ij > list of lists 
 g = [[0]*(M+1)]*N # g_ij > Cost
@@ -45,14 +45,17 @@ g = [[0]*(M+1)]*N # g_ij > Cost
 def randomMememex():
 	f = 0 # Total cost of allocation
 	j = 0 # Random variable
-	for i in range(1, N+1):
+	lu = len(u)
+	lc = len(c)
+	for i in s:
 		while True:
-			j = random.randint(1, M+1)
-			try:
-				if (u[j] + s[i] <= c[j]):
+			#j = random.randint(1, M+1)
+			for j in range(1, M+2):
+				print "size %d %d   length: %d %d "%(i,j,lu,lc)
+				
+				if (u[j] + i <= c[j]):
+					continue
 					break
-			except IndexError as e:
-				print("Error! Index i, j: ", i, " ", j, " %s"%e)
 		X[i][j] = 1
 		u[j] += s[i]
 		#XXX Calculate g[i][j] (cost)
