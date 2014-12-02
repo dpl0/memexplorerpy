3c74e3901cd02c1592df25240c188cbaf661af96
dplbsd <tuchalia@gmail.com>
2014-12-02 15:58:29 +0100

    Remove geninput.py

--- dev/geninput.py
+++
@@ -1,8 +0,0 @@
-# This program generates data for the memexplorer problem.
-# It should write 2 lines, one with memory  banks definitions,
-# the other one with data structures definitions, separated by comma.
-
-# It should use the format:
-# '14, 15, 16, 19, 39\n' (Memory banks)
-# '12', 12, 10, 8' (Data Structures)
-#
