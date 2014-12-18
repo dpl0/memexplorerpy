fa91da8e49f6a934b949b3a41456642c2121923b
dplbsd <tuchalia@gmail.com>
2014-12-19 00:15:28 +0100

    Finished with BGRKA, took much longer than expected.

--- dev/brkga.py
+++ dev/brkga.py
@@ -29,7 +29,12 @@ class brkga:
 
 	# Populations
 	previous = []
-	ranking = []
+	current = []
+
+	# Auxiliar function.
+	def clear_rank(self, pop):
+		return [i[1] for i in pop]
+		
 
 	def __init__(self, dec, n=100, p=1000, pe=0.20, pm=0.10, rhoe=0.70, s=0):
 		self.n = n
@@ -37,105 +42,106 @@ class brkga:
 		self.pe = pe
 		self.pm = pm 
 		self.rhoe =rhoe 
-		self.dec = dec 
+		self.decoder = dec 
 		self.seed = s
 		
-		self.previous = self.create_population(p, n)
-		self.ranking = self.rank_solutions(self.previous)
+		self.current = self.create_population(p, n)
+		self.previous = list(self.current)
 		return
 
 	# Creates the whole set of population.
 	def create_population(self, p, n):
-		pop = []
-		for i in range(p):
-			chrom = self.create_chromosome(n)
-			pop.append(chrom)
-		return pop
+		return sorted([self.create_chromosome(n) for i in range(p)])
 	
 	# Each chromosome is a list of random bits.
+	# They have to be correct.
 	def create_chromosome(self, n):
 		while True:
 			number = random.getrandbits(n)
-			chromosome = [True if i == '1' else False for i in bin(number)[2:]]
-			while len(chromosome) < n:
-				chromosome = [False] + chromosome
+			chrom = [True if i == '1' else False for i in bin(number)[2:]]
+			while len(chrom) < n:
+				chrom = [False] + chrom
 
-			if self.dec(chromosome) != False: return chromosome
-		return chromosome
-
-	def rank_solutions(self, pop):
-		rank = [(self.dec(p), p) for p in pop]
-		rank = sorted(rank)
-		return rank
+			rankedchrom = (self.decoder(chrom), chrom)
+			if rankedchrom[0] != False:
+				return rankedchrom
 
 	# Evolve the current population.
 	def evolve(self):
-		# 6 - Copy elite solutions to next population.
-		elite, nonelite = self.getElite(self.pe, len(self.ranking))
-		# 7 - Generate mutant solutions.
-		mutants = self.mutate(self.pm, self.previous)
-		# 8 - Crossover
-		crossover = self.crossover(self.rhoe, elite, nonelite, self.p-len(elite)-len(mutants))
-		self.previous = elite + crossover + mutants
-		self.ranking = self.rank_solutions(self.previous)
+		elite, nonelite = self.getElite()
+		mutants = self.mutate()
+
+		cossoverelem = self.p - len(elite) - len(mutants)
+		crossover = self.crossover(elite, nonelite, cossoverelem)
+
+		self.current = sorted(elite + crossover + mutants)
+		self.previous = list(self.current)
 		return
 
 	# return elite, nonelite
-	def getElite(self, pe, poplen):
-		eltotake = int(poplen * pe)
-		elite = self.ranking[:eltotake]
-		nonelite = self.ranking[eltotake:]
-		
-		# Take out of rank.
-		cleanelite = [i[1] for i in elite]
-		cleannonelite = [i[1] for i in nonelite]
-		return cleanelite, cleannonelite
+	def getElite(self):
+		eltotake = int(self.n * self.pe)
+		elite = self.previous[:eltotake]
+		nonelite = self.previous[eltotake:]
+		return elite, nonelite
 
-	# Mutate p% elements of pop.
-	def mutate(self, p, pop):
+	# Mutate p% elements of pop at random.
+	def mutate(self):
 		mutants = []
-		for i in range(len(pop)):
-			if random.random() <= p:
-				while True:
-					for j in pop[i]:
-						for k in pop[j]:
-							if random.random() <= 0.5:
-								rand = random.random()
-								if rand <= 0.5:
-									pop[j][k] = False
-								else: pop[j][k] = True
-					if self.dec(pop[i]) != False: break
-				mutants.append(pop[i])
+		for i in range(self.p):
+			if random.random() < self.pm:
+				mutants.append(self.create_mutant(i))
 		return mutants
+	
+	def create_mutant(self, index):
+		while True:
+			mutant = []
+			for v in self.previous[index][1]:
+				if random.random() <= 0.5:
+					mutant.append(not v)
+				else: mutant.append(v)
+
+			ret = (self.decoder(mutant), mutant)
+			# Check correctness of chromosome.
+			if ret[0] != False:
+				return ret
 
 	# Crossover the specified number of elements.
 	# Returns the created elements.
-	def crossover(self, rhoe, elite, nonelite, elems):
+	def crossover(self, elite, nonelite, elems):
 		ret = []
 		for i in range(elems):
 			while True:
 				ei = random.randint(0, len(elite)-1)
 				ni = random.randint(0, len(nonelite)-1)
-				newm= []
-				for i in range(len(elite[ei])):
-					if random.random() <= rhoe: newm.append(elite[ei][i])
-					else: newm.append(nonelite[ni][i])
-				if self.dec(newm) != False:
-					ret.append(newm)
+
+				newelem = []
+				for i in range(self.n):
+					if random.random() <= self.rhoe:
+						newelem.append(elite[ei][1][i])
+					else: newelem.append(nonelite[ni][1][i])
+				rankedelem = (self.decoder(newelem), newelem)
+				if rankedelem[0] != False:
+					ret.append(rankedelem)
 					break
 		return ret
-	
-	def bestSolution(self):
-		return self.ranking[0]
 
+	def bestSolution(self):
+		return self.current[0]
 
 # Takes a chromosome as input.
-# Returns it fitness (cost of the problem).
+# Returns False if the solution is unfeasible, or its cost.
 def decoder(ch):
 	matrixlen = len(problem.X)
 	rowlen = len(problem.X[0])
 
-	matrix = [[ch[i*rowlen+j] for j in range(rowlen)] for i in range(matrixlen)]
+	#matrix = [[ch[i*rowlen+j] for j in range(rowlen)] for i in range(matrixlen)]
+	matrix = []
+	for i in range(matrixlen):
+		row = []
+		for j in range(rowlen):
+			row.append(ch[i*rowlen + j])
+		matrix.append(row)
 
 	problem.X = matrix
 	if problem.is_correct():
@@ -154,12 +160,11 @@ def matrix_len(matrix):
 problem = memproblem.read_problem("./test.dat")
 
 if __name__ == "__main__":
-	generations = 1000
+	generations = 500
 
-	brkgasolver = brkga(dec=decoder, n=matrix_len(problem.X))
+	brkgasolver = brkga(dec=decoder, n=matrix_len(problem.X), p=10)
 
 	for i in range(generations):
 		brkgasolver.evolve()
-	a = brkgasolver.bestSolution()
-	print a
+	print brkgasolver.bestSolution()
 
