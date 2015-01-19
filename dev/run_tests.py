e8de444fe6e6691db28decd1d14b7cda48cac016
dpl <tuchalia@gmail.com>
2015-01-19 19:41:34 +0100

    Almost finished testing script. (WIP)

--- dev/run_tests.py
+++ dev/run_tests.py
@@ -4,8 +4,8 @@ import sys, time, timeit
 sys.path.append("../dev/")
 import brkga, grasp, memproblem, tabumemex
 
-def get_code(mh, filename, iters, pars):
-	"""Creates code templates using parameters."""
+def get_grasp_code(mh, filename, iters):
+	"""Creates code templates."""
 	if mh == 'grasp':
 		command = "print grasp(problem,1,"+str(iters)+")"
 		code = """ 
@@ -14,76 +14,81 @@ from grasp import grasp
 
 problem = memproblem.read_problem('"""+filename+"""')
 """
-		return command, code
+	return command, code
 
-	elif mh == 'tabu':
-		command = "print cost"
-		code = """
+def get_tabu_code(mh, filename, iters):
+	command = "print cost"
+	code = """
 import memproblem
 import tabumemex
 
 problem = memproblem.read_problem('"""+filename+"""')
 cost = tabumemex.tabumemex(problem, 100)
 """
-		return command, code
+	return command, code
 
-	elif mh == 'brkga':
-		command = "print brk.bestSolution(problem,1,"+str(iters)+").results"
-		command = "print brkgasolver.bestSolution()"
-		code = """
+def get_brkga_code(mh, filename, iters):
+	command = "print brkgasolver.bestSolution()[0]"
+	code = """
 import memproblem, brkga
 
 problem = memproblem.read_problem('"""+filename+"""')
-brkgasolver = brkga.brkga(n=problem.datastructs_n, p=20, s=time.time(), dec=brkga.decoder)
+n = problem.datastructs_n
+s = time.time()
+brkgasolver = brkga.brkga(problem, brkga.decoder, n, 20, s)
 for i in range("""+str(iters)+"""):
 	brkgasolver.evolve()
 """
-		return command, code
-
-	else:
-		return None, None
+	return command, code
 
 class execute():
 	"""Execute the problems with the different metaheuristics."""
-	def __init__(self, problem, iters):
+	def set_problem(self, prob):
+		self.problem = prob
+
+	def __init__(self, iters, rep=4):
 		self.iters = iters
-		self.problem = problem
-		self.repeats = 4
+		self.repeats = rep
 		return
 	
-	def run_grasp(self, filename, pars=None):
-		print("GRASP ("+str(self.iters)+")")
+	def run_grasp(self, filename, prob=None):
+		self.set_problem(prob)
+		print("GRASP ("+filename+", "+str(self.iters)+")")
 
-		command, code = get_code('grasp', filename, self.iters, pars)
+		command, code = get_grasp_code('grasp', filename, self.iters)
 
-		grasp_time = timeit.timeit(command, code, number=self.repeats)
-		print("Avg execution time: "+str(grasp_time/self.repeats))
+		exec_time = timeit.timeit(command, code, number=self.repeats)
+		print("Avg execution time: "+str(exec_time/self.repeats))
 		return
 	
-	def run_tabu(self, filename, pars=None):
-		print("Tabu Search ("+str(self.iters)+")")
+	def run_tabu(self, filename, prob=None):
+		self.set_problem(prob)
+		print("Tabu Search ("+filename+", "+str(self.iters)+")")
 
-		command, code = get_code('tabu', filename, self.iters, pars)
-		tabu_time = timeit.timeit(command, code, number=self.repeats)
-		print("Avg execution time: "+str(tabu_time/self.repeats))
+		command, code = get_tabu_code('tabu', filename, self.iters)
+		exec_time = timeit.timeit(command, code, number=self.repeats)
+		print("Avg execution time: "+str(exec_time/self.repeats))
 		return
 	
-	def run_brkga(self, filename, pars=None):
-		print("BRKGA ("+str(self.iters)+")")
+	def run_brkga(self, filename, prob=None):
+		self.set_problem(prob)
+		print("BRKGA ("+filename+", "+str(self.iters)+")")
 
-		command, code = get_code('grasp', filename, self.iters, pars)
-		brkga_time = timeit.timeit(command, code, number=self.repeats)
-		print("Avg execution time: "+str(brkga_time/self.repeats))
+		command, code = get_brkga_code('grasp', filename, self.iters)
+		exec_time = timeit.timeit(command, code, number=self.repeats)
+		print("Avg execution time: "+str(exec_time/self.repeats))
 		return
 
 
 if __name__ == "__main__":
-	fname = sys.argv[1]
-	problem = memproblem.read_problem(fname)
-	# Iters: iterations for GRASP, tabu memex, brkga. 
-	iters = int(sys.argv[2])
-	executor = execute(problem, iters)
-	
-	executor.run_grasp(fname)
-	executor.run_tabu(fname)
-	executor.run_brkga(fname)
+	iters = int(sys.argv[1])
+	fnames = sys.argv[2:]
+
+	executor = execute(iters)
+
+	for fname in fnames:
+		problem = memproblem.read_problem(fname)
+		executor.run_grasp(fname, problem)
+		executor.run_tabu(fname, problem)
+		executor.run_brkga(fname, problem)
+		print 
