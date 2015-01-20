2ecffec6faec9c790c3fcf6cc32825b3a207ee99
dpl <tuchalia@gmail.com>
2015-01-20 13:11:44 +0100

    Corrected partially testit usage.

--- dev/run_tests.py
+++ dev/run_tests.py
@@ -4,11 +4,10 @@ import sys, time, timeit
 sys.path.append("../dev/")
 import brkga, grasp, memproblem, tabumemex
 
-def get_grasp_code(mh, filename, iters):
+def get_grasp_code(filename, iters):
 	"""Creates code templates."""
-	if mh == 'grasp':
-		command = "print grasp(problem,1,"+str(iters)+")"
-		code = """ 
+	command = "print grasp(problem,1,"+str(iters)+")"
+	code = """ 
 import memproblem
 from grasp import grasp
 
@@ -16,30 +15,31 @@ problem = memproblem.read_problem('"""+filename+"""')
 """
 	return command, code
 
-def get_tabu_code(mh, filename, iters):
-	command = "print cost"
+def get_tabu_code(filename, iters):
+	command = "print tabumemex.tabumemex(problem, 100)"
 	code = """
 import memproblem
 import tabumemex
 
 problem = memproblem.read_problem('"""+filename+"""')
-cost = tabumemex.tabumemex(problem, 100)
 """
 	return command, code
 
-def get_brkga_code(mh, filename, iters):
-	command = "print brkgasolver.bestSolution()[0]"
+def get_brkga_code(filename, iters):
+	command = """
+n = problem.datastructs_n
+s = time.time()
+# 20 is population size.
+brkgasolver = brkga.brkga(problem, brkga.decoder, n, 20, s)
+for i in range("""+str(iters)+"""):
+	brkgasolver.evolve()
+print brkgasolver.bestSolution()[0]
+"""
+
 	code = """
 import memproblem, brkga
 
 problem = memproblem.read_problem('"""+filename+"""')
-n = problem.datastructs_n
-s = time.time()
-brkgasolver = brkga.brkga(problem, brkga.decoder, n, 20, s)
-for i in range("""+str(iters)+"""):
-	brkgasolver.evolve()
-	if i % 50 == 0:
-		print brkgasolver.bestSolution()[0]
 """
 	return command, code
 
@@ -56,9 +56,7 @@ class execute():
 	def run_grasp(self, filename, prob=None):
 		self.set_problem(prob)
 		print("GRASP ("+filename+", "+str(self.iters)+")")
-
-		command, code = get_grasp_code('grasp', filename, self.iters)
-
+		command, code = get_grasp_code(filename, self.iters)
 		exec_time = timeit.timeit(command, code, number=self.repeats)
 		print("Avg execution time: "+str(exec_time/self.repeats))
 		return
@@ -66,8 +64,7 @@ class execute():
 	def run_tabu(self, filename, prob=None):
 		self.set_problem(prob)
 		print("Tabu Search ("+filename+", "+str(self.iters)+")")
-
-		command, code = get_tabu_code('tabu', filename, self.iters)
+		command, code = get_tabu_code(filename, self.iters)
 		exec_time = timeit.timeit(command, code, number=self.repeats)
 		print("Avg execution time: "+str(exec_time/self.repeats))
 		return
@@ -75,8 +72,7 @@ class execute():
 	def run_brkga(self, filename, prob=None):
 		self.set_problem(prob)
 		print("BRKGA ("+filename+", "+str(self.iters)+")")
-
-		command, code = get_brkga_code('grasp', filename, self.iters)
+		command, code = get_brkga_code(filename, self.iters)
 		exec_time = timeit.timeit(command, code, number=self.repeats)
 		print("Avg execution time: "+str(exec_time/self.repeats))
 		return
